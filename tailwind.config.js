78cba2c0ce3aefb4cd2c6eaadfdf2f08e40fb05c
Robert Corponoi <robertcorponoi@gmail.com>
2022-02-27 17:23:07 -0800

    Updated tailwind config for tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 /** @type {import("tailwindcss/tailwind-config").TailwindConfig} */
 module.exports = {
-    // An array of paths to the template files that should be purged of unused styles.
-    // https://tailwindcss.com/docs/optimizing-for-production
-    purge: ["./src/**/*.js", "./src/**/*.ts", "./src/**/*.tsx"],
-    // If set to media, dark mode classes, like `dark:text-white` will take precedence over unprefixed classes.
-    // If set to class, dark mode classes will be applied whenever the `dark` class is present in the HTML tree.
-    // https://tailwindcss.com/docs/dark-mode
-    darkMode: "media",
+    // An array of paths to the template files that use Tailwind class names.
+    // https://tailwindcss.com/docs/content-configuration
+    content: [
+        "./src/**/*.js",
+        "./src/**/*.ts",
+        "./src/**/*.tsx",
+    ],
     theme: {
-        extend: {},
-    },
-    variants: {
         extend: {},
     },
     plugins: [],
