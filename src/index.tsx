631914dce62ff0f37d449774c1c63f83d81a29f0
Robert Corponoi <robertcorponoi@gmail.com>
2023-10-29 18:00:50 -0400

    Switched from BrowserRouter component to createBrowserRouter and added the Outlet in App.tsx where the pages should be rendered

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,61 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
+import HomePage from "./pages/HomePage";
+import ReduxPage from "./pages/ReduxPage";
+import AboutPage from "./pages/AboutPage";
+import NotFoundPage from "./pages/404Page";
+
 import { store } from "./store/store";
 
-/**
- * Tells React what to render and where to render it.
- *
- * In our case, we're rending our root `App` component to the DOM element with
- * the id of `root` in the `public/index.html` file.
- */
+// Creates the routes for the app.
+export const router = createBrowserRouter([
+    {
+        /**
+         * The `App` component contains the template used by every page page 
+         * so all pages will be rendered inside of it. There is an 
+         * `<Outlet />` component inside of it that determines where the 
+         * content of each page will be rendered.
+         */
+        path: "/",
+        element: (
+            <App />
+        ),
+        children: [
+            {
+                path: "",
+                element: (
+                    <HomePage />
+                ),
+            },
+            {
+                path: "redux",
+                element: (
+                    <ReduxPage />
+                ),
+            },
+            {
+                path: "about",
+                element: (
+                    <AboutPage />
+                ),
+            },
+        ]
+    },
+    {
+        /**
+         * This route will be rendered if no other route matches the URL.
+         */
+        path: "*",
+        element: (
+            <NotFoundPage />
+        )
+    }
+])
 
 // The container to mount the React app to.
 const container = document.getElementById("root");
@@ -22,8 +66,8 @@ const root = createRoot(container!);
 
 root.render(
     <React.StrictMode>
-    <Provider store={store}>
-        <App />
-    </Provider>
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
     </React.StrictMode>
 );
