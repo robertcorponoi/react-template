ca16fd58180974c804bbcdc86cced8440cf47765
Robert Corponoi <robertcorponoi@gmail.com>
2024-05-19 17:25:22 -0400

    Updated dependencies with major version changes. Couldn't update eslint to the latest version since @typescript/eslint packages need ^8.56.0. Removed redux-thunk since it's automatically added by @reduxjs/toolkit

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,3 @@
-import thunk from "redux-thunk";
 import { configureStore } from "@reduxjs/toolkit";
 
 import userSlice from "./user/userSlice";
@@ -11,13 +10,6 @@ export const store = configureStore({
     reducer: {
         user: userSlice,
     },
-    middleware: [
-        // Redux-thunk allows us to write action creators that return a function
-        // instead of an action. This can be used to delay the dispatch of an
-        // action or dispatch only if a certain condition is met.
-        // https://github.com/reduxjs/redux-thunk
-        thunk,
-    ],
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself.
